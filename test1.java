3903b33217b34dc00e12efbe26382aa1a003f816
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-03-12 17:59:19 +0900

    Update test1.java
    1. 큰솔 화면에 구구단 출력하기

--- test1.java
+++ test1.java
@@ -1,9 +1,10 @@
+// 1. 큰솔 화면에 구구단 출력하기
 public class test1 {
     public static void main(String[] args) {
-        // 구구단 출력 JAVA의 다중 반복문과 format함수를 사용
+        System.out.println("[구구단 출력]");
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
-                System.out.printf(String.format("%02d x %02d =%02d ", i, j, i * j));
+                System.out.printf(String.format("%02d x %02d = %02d\t", i, j, i * j));
             }
             System.out.println();
         }
