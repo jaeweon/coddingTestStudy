0d31449edca50ba38109f20eb43ef0fa1b0a7737
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-03-21 23:08:05 +0900

    Delete BaekJoon_10926.java

--- BaekJoon_10926.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class BaekJoon_10926 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String id = sc.next();
-        System.out.println(id+"??!");
-    }
-}
