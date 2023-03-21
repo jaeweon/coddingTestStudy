7cee6777cf0044490b7f6b83a5a69b1eb3acadc2
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-03-21 23:06:37 +0900

    Add files via upload

---
+++ BaekJoon_1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BaekJoon_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+
+        System.out.println(A / B);
+    }
+}
