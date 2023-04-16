cebb388975a0efc5bac61ae5bc20b7d86f53c973
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-04-16 21:32:16 +0900

    부족한 금액 계산하기
    부족한 금액 계산하기

---
+++ prac1.java
@@ -0,0 +1,15 @@
+package Programmers_LV1;
+
+public class prac1 {
+    // 부족한 금액 계산하기
+        public long solution(int price, int money, int count) {
+            long answer = 0;
+            long sum = 0;
+            for(int i = 1; i < count + 1; i++){
+                sum += price * i;
+            }
+            if(sum > money) answer = sum - money;
+            return answer;
+        }
+    }
+
